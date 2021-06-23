c9e89149fd1ea4ce4033878d90394c45727c45d0
ShehrozAli <shehrozali77@live.com>
2021-06-23 16:42:55 +0500

    new

--- Deployment_2.sh
+++ Deployment_2.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-git clone https://github.com/ShehrozAli77/LMS2.git
-cd LMS2/
-docker build -t node-mysql-app .
-echo "Launching Mysql Service"
-docker-compose up -d
-sleep 15
-echo "Launching App"
-docker run --name library_sys -p 3000:3000 node-mysql-app
+kubectl create -f PersistentVolume.yaml
+kubectl create -f storage.yaml
+kubectl apply -f mysql2.yaml
+kubectl apply -f node.yaml
+kubectl get pods
